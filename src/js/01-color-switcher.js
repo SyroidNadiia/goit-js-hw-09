41992d37948914270a933ec3298a90c5b2c379f1
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-02-19 13:16:32 +0200

    add task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+btnStartEl = document.querySelector('button[data-start]');
+btnStopEl = document.querySelector('button[data-stop]');
+
+btnStartEl.addEventListener('click', onClickStart);
+btnStopEl.addEventListener('click', onClickStop);
+btnStartEl.disabled = false;
+btnStopEl.disabled = true;
+
+let timerID = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStart(event) {
+  timerID = setInterval(onChangeColor, 1000);
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+}
+
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onClickStop(event) {
+  clearInterval(timerID);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+}
