b7e14f65fd5af2b4fa22e55c5f3a1db526e3b490
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-02-21 18:02:36 +0200

    fixed task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,47 +1,61 @@
+import Notiflix from 'notiflix';
+
 refs = {
+  form: document.querySelector('.form'),
   delay: document.querySelector('input[name="delay"]'),
   step: document.querySelector('input[name="step"]'),
-  // amount: document.querySelector('input[name="amount"]'),
-  btnSubmit: document.querySelector('button[type="submit"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  btn: document.querySelector('button[type="submit"]'),
 };
 
-refs.delay.addEventListener('input', onEnteredDelay);
-refs.step.addEventListener('input', onEnteredStep);
-// refs.amound.addEventListener('input', onEnteredAmount);
-refs.btnSubmit.addEventListener('submit', onClickBtn);
-
-let position = 0;
-let delay = 0;
-
-function onEnteredDelay(event) {
-  return event.target.value;
-}
-
-function onEnteredStep(event) {
-  const valueStep = event.target.value;
-  console.log(valueStep);
-}
-
-function onEnteredAmount() {}
-
-function onClickBtn(event) {
+refs.form.addEventListener('submit', event => {
   event.preventDefault();
+});
+refs.delay.addEventListener('input', event => {
+  delay = Number(event.target.value);
+});
+refs.step.addEventListener('input', event => {
+  step = Number(event.target.value);
+});
+refs.amount.addEventListener('input', event => {
+  amount = event.target.value;
+});
+refs.btn.addEventListener('click', onClickBtn);
+
+const formData = {};
+let delay = 0;
+let step = 0;
+let amount = 0;
+let position = 0;
+
+function onClickBtn() {
+  if (position >= amount) {
+    return;
+  }
   position += 1;
-  delay = onEnteredDelay();
-  createPromise(position, delay);
+
+  const promises = createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  delay += step;
+  onClickBtn();
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
       }
-      reject(`❌ Rejected promise ${position} in ${delay}ms`);
     }, delay);
   });
 }
-
-// 
